ea99dfbb5d766adc692e0cd2c9c8a24e3ea8d382
Mariana Caldas <marianacaldas94@gmail.com>
2024-02-27 10:41:10 -0300

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <p>
+                    <span>Desenvolvido por Mariana</span> @ 2024
+                </p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
